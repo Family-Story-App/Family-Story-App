961ef2bbd8c087df9bc1d1d2aec4ef3d735036e6
M-Doja <micdoja@yahoo.com>
2015-10-28 14:40:35 -0700

    updating progress, buttons on story shown

--- public/javascript/controllers/StoryDetailsController.js
+++ public/javascript/controllers/StoryDetailsController.js
@@ -6,27 +6,47 @@
 	angular.module('app')
 	.controller('StoryDetailsController', StoryDetailsController);
 	function StoryDetailsController(HomeFactory, $stateParams, $state) {
-
 		var vm = this;
-		// vm.title = 'Welcome to our App!';
 		vm.astory = {};
 		vm.Story = [];
+		vm.comment = {};
 		vm.showComment = false;
 		vm.NoComment = false;
 		vm.YesComment = false;
 
+
+ 	console.log('running');
 		if(!$stateParams.id) $state.go('Home');
 		HomeFactory.getStoryById($stateParams.id).then(function(res){
 			console.log(res);
 			vm.astory = res;
 		});
 
+vm.EditStory = function(){
+
+};
 
 	vm.addCom = function(){
-		HomeFactory.postCom().then(function(res){
-
+			console.log("adding comment");
+		HomeFactory.postCom(vm.comment).then(function(res){
+				console.log('comment added');
+			vm.comment = res.body;
+			console.log(res.body);
+			if(!$stateParams.id) $state.go('Home');
+			HomeFactory.getStoryById($stateParams.id).then(function(res){
+				console.log(res);
+				vm.astory = res;
+			});
+		});
+		vm.comment = {};
+	};
+	vm.showCom = function(){
+		HomeFactory.displayCom(vm.comment).then(function(res){
+			vm.comment = res;
+			console.log(res);
 		});
 	};
+
 
 	vm.ShowCom = function(){
 		vm.showComment = true;
